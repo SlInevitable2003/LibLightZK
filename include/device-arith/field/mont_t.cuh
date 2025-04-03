0fa69ca8cbf4e87275a89f8ebf18ec2e49b5cfb8
Lu <u202114710@hust.edu.cn>
2025-04-03 19:38:31 +0800

    Little change

--- include/device-arith/field/mont_t.cuh
+++ include/device-arith/field/mont_t.cuh
@@ -46,20 +46,20 @@ protected:
 private:
     uint32_t even[n];
 
-    static inline void mul_n(uint32_t *acc, const uint32_t *a, uint32_t bi, size_t n = n)
+    static inline void mul_n(uint32_t *acc, const uint32_t *a, uint32_t bi, size_t n = (N + 31) / 32)
     {
         for (size_t j = 0; j < n; j += 2)
             asm("mul.lo.u32 %0, %2, %3; mul.hi.u32 %1, %2, %3;" : "=r"(acc[j]), "=r"(acc[j + 1]) : "r"(a[j]), "r"(bi));
     }
 
-    static inline void cmad_n(uint32_t *acc, const uint32_t *a, uint32_t bi, size_t n = n)
+    static inline void cmad_n(uint32_t *acc, const uint32_t *a, uint32_t bi, size_t n = (N + 31) / 32)
     {
         asm("mad.lo.cc.u32 %0, %2, %3, %0; madc.hi.cc.u32 %1, %2, %3, %1;" : "+r"(acc[0]), "+r"(acc[1]) : "r"(a[0]), "r"(bi));
         for (size_t j = 2; j < n; j += 2)
             asm("madc.lo.cc.u32 %0, %2, %3, %0; madc.hi.cc.u32 %1, %2, %3, %1;" : "+r"(acc[j]), "+r"(acc[j + 1]) : "r"(a[j]), "r"(bi));
     }
 
-    static inline void cadd_n(uint32_t *acc, const uint32_t *a, size_t n = n)
+    static inline void cadd_n(uint32_t *acc, const uint32_t *a, size_t n = (N + 31) / 32)
     {
         asm("add.cc.u32 %0, %0, %1;" : "+r"(acc[0]) : "r"(a[0]));
         for (size_t i = 1; i < n; i++)
@@ -87,7 +87,7 @@ private:
         inline wide_t() {}
 
     private:
-        static inline void mad_row(uint32_t *odd, uint32_t *even, const uint32_t *a, uint32_t bi, size_t n = n)
+        static inline void mad_row(uint32_t *odd, uint32_t *even, const uint32_t *a, uint32_t bi, size_t n = (N + 31) / 32)
         {
             cmad_n(odd, a + 1, bi, n - 2);
             asm("madc.lo.cc.u32 %0, %2, %3, 0; madc.hi.u32 %1, %2, %3, 0;" : "=r"(odd[n - 2]), "=r"(odd[n - 1]) : "r"(a[n - 1]), "r"(bi));
